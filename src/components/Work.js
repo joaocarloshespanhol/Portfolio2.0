d463f8e16e5b70104ddb77277e7ec6e8a8130380
joaocarloshespanhol <joaocarlos.hespanhol@hotmail.com>
2025-11-12 21:12:53 -0300

    Atualiza projeto na pagina work

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,7 +9,7 @@ import Img5 from '../assets/portfolio-img5.png';
 import Img6 from '../assets/portfolio-img6.png';
 import Img7 from '../assets/portfolio-img7.png';
 import Img8 from '../assets/portfolio-img8.png';
-// import Img9 from '../assets/portfolio-img9.png';
+import Img9 from '../assets/portfolio-img9.png';
 
 const Work = () => {
   return ( 
@@ -156,20 +156,20 @@ const Work = () => {
                 <span className='text-gradient text-[12px] lg:text-xl text-white'> www.gabrielledelatorre.adv.br </span>
               </div></a>
           </div>
-          {/* <div className='mt-7 group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+          <div className='mt-7 group relative overflow-hidden border-2 border-white/50 rounded-xl'>
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'> </div>
 
-              <a href='https://agend-flow.vercel.app/'
+              <a href='https://www.jcdesenvolvedorweb.com.br/'
               target='_blank'
               rel="noopener noreferrer">
               <img className='group-hover:scale-125 transition-all duration-500' src={Img9} alt="" />
               <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <span className='text-gradient text-[20px] lg:text-[30px] text-[#42A6E3]'> Sistema de agendamento. </span>
+                <span className='text-gradient text-[20px] lg:text-[30px] text-[#42A6E3]'> Site da minha empresa. </span>
               </div>
               <div className='absolute -bottom-full left-12 group-hover:bottom-16 transition-all duration-700 z-50'>
-                <span className='text-gradient text-[12px] lg:text-xl text-white'> www.agendflow.vercel.app </span>
+                <span className='text-gradient text-[12px] lg:text-xl text-white'> www.jcdesenvolvedorweb.com.br </span>
               </div></a>
-          </div> */}
+          </div>
           
         </div>
       </motion.div>
